1fe9a32dc999ed29c4e596e56d294ace41d2c977
Trixie Samson <trixiemaebsamson@iskolarngbayan.pup.edu.ph>
2022-11-11 20:54:20 +0800

    Initial Commit
    .gitignore
    .gitattributes

---
+++ Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float speed = 15.0f;
+    private float rotationSpeed = 75.0f;
+    private float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        // Get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // Tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime * verticalInput);
+    }
+}
---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Private Variables
+    private float speed = 5.0f;
+    private float turnSpeed = 20.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        // Moves the car forward based on Vertical Input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on Horizontal Input
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
+// Trixie Mae Samson | BSCOE 2-1 
